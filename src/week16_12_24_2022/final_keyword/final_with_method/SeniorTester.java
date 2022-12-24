8c8540d2126e69dc3dc9514f4008f8382fd3e3ac
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-24 18:49:43 +0300

    week16 Final_Method created

---
+++ src/week16_12_24_2022/final_keyword/final_with_method/SeniorTester.java
@@ -0,0 +1,21 @@
+package week16_12_24_2022.final_keyword.final_with_method;
+
+import week15_12_17_2022.method_overriding.ParentClass;
+
+public class SeniorTester {
+
+    final public void seniorCodes(){
+
+        System.out.println("DO NOT CHANGE TO IMPLEMENTATION");
+    }
+
+    public void junior1(){
+        // TODO you will create your job
+    }
+    public void junior2(){
+        // TODO you will create your job
+    }
+    public void junior3(){
+        // TODO you will create your job
+    }
+}
