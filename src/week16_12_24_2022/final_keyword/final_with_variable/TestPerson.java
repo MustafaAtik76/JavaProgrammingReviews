ab4e687e60f2b2235e4b14b11435a09856b140e0
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-24 18:41:38 +0300

    week16 Final_Variable created

---
+++ src/week16_12_24_2022/final_keyword/final_with_variable/TestPerson.java
@@ -0,0 +1,18 @@
+package week16_12_24_2022.final_keyword.final_with_variable;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+
+        Person person = new Person("14-12-1998", "ADam");
+
+        System.out.println(person);
+
+        person.setName("Adam");
+
+        System.out.println(person);
+
+
+
+    }
+}
